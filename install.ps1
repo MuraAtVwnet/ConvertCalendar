b5d92a59d7c5af2501fdd8eeba64eab85a46e9d1
Syuichi Murashima <mura@vwnet.jp>
2024-03-10 11:22:11 +0900

    互換性確保

--- install.ps1
+++ install.ps1
@@ -2,7 +2,10 @@
 $ModuleName = "ConvertCalendar"
 
 # Module Path
+if(($PSVersionTable.Platform -eq "Win32NT") -or ($PSVersionTable.Platform -eq $null){
 $ModulePath = Join-Path (Split-Path $PROFILE -Parent) "Modules"
+else{
+Join-Path ($HOME) "/.local/share/powershell/Modules"}
 $NewPath = Join-Path $ModulePath $ModuleName
 
 # Make Directory
